2d1c1084dcc9579d6167973034dfa56f81fc264a
Alexander Egorov <egoroff@gmail.com>
2020-12-08 11:21:27 +0300

    Brew formula update for copyto version v1.1.1

--- copyto.rb
+++ copyto.rb
@@ -2,24 +2,24 @@
 class Copyto < Formula
   desc "A small command line app written in Go that allows you to easily one way sync between folders"
   homepage "https://github.com/aegoroff/copyto"
-  version "1.1.0"
+  version "1.1.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/aegoroff/copyto/releases/download/v1.1.0/copyto_1.1.0_darwin_amd64.tar.gz"
-    sha256 "f3ca10cc04a13ce0dcee8b909d979614273ad57e60c555c8dd67942ea91ab67d"
+    url "https://github.com/aegoroff/copyto/releases/download/v1.1.1/copyto_1.1.1_darwin_amd64.tar.gz"
+    sha256 "dd89c5916b88559eb951b043e286828cb355dc5138e07cd43c750609b3bd5065"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/aegoroff/copyto/releases/download/v1.1.0/copyto_1.1.0_linux_amd64.tar.gz"
-      sha256 "f0532bed15b17951b834236d06585d9052d200c8895541032f50f83502fecf48"
+      url "https://github.com/aegoroff/copyto/releases/download/v1.1.1/copyto_1.1.1_linux_amd64.tar.gz"
+      sha256 "732dc404e4a73ee91120292b9301ca0931d3428e69979e636f20267dd7e83770"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/aegoroff/copyto/releases/download/v1.1.0/copyto_1.1.0_linux_arm64.tar.gz"
-        sha256 "69589c1cc630de4052fb6658b9c5bafd97cda56110b86f5f439f2ea5725e3d79"
+        url "https://github.com/aegoroff/copyto/releases/download/v1.1.1/copyto_1.1.1_linux_arm64.tar.gz"
+        sha256 "7e392aa108dd4465151ae4103690c47ed701d26de74b38d22dedf0ff879e345e"
       else
-        url "https://github.com/aegoroff/copyto/releases/download/v1.1.0/copyto_1.1.0_linux_armv7.tar.gz"
-        sha256 "bc198968f180d3bd349b55c6f9c7fc882f39ae4ccd2a8c79de408af44e187654"
+        url "https://github.com/aegoroff/copyto/releases/download/v1.1.1/copyto_1.1.1_linux_armv7.tar.gz"
+        sha256 "ec1fd2d19fa7ce5e26cc8b1560e006d6c2d2f1c7606ab713f0d6f1efce33dfa7"
       end
     end
   end
