a8547353c6ded9898425f1e1937909b396a6b7b1
Alexander Egorov <egoroff@gmail.com>
2021-10-16 09:30:21 +0300

    Brew formula update for fg version v0.2.5

--- fg.rb
+++ fg.rb
@@ -5,33 +5,38 @@
 class Fg < Formula
   desc "Small console app written in Go that allows you to easily group all files in the dir specified"
   homepage "https://github.com/aegoroff/fg"
-  version "0.2.4"
+  version "0.2.5"
   license "Apache-2.0"
   bottle :unneeded
 
-  if OS.mac?
-    url "https://github.com/aegoroff/fg/releases/download/v0.2.4/fg_0.2.4_darwin_amd64.tar.gz"
-    sha256 "ad412a1dc1afaaea80c17b0d0c92c1ed8ecfaba92439da52f20a1fc771ce32ac"
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/aegoroff/fg/releases/download/v0.2.5/fg_0.2.5_darwin_amd64.tar.gz"
+      sha256 "b569416a0928fcf5730154b6350da3330a924a770790bb4eda5395c1c6483d52"
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/aegoroff/fg/releases/download/v0.2.5/fg_0.2.5_darwin_arm64.tar.gz"
+      sha256 "bf22a33fafc798904c8922a798486e5b098a40d4898154ecc550e63a0b0cf689"
+    end
   end
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/aegoroff/fg/releases/download/v0.2.4/fg_0.2.4_darwin_arm64.tar.gz"
-    sha256 "348c88df622acf87ee153994d3b9fa8fe6d2d47e848f3ce30e2ee4389a0f28d0"
-  end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/aegoroff/fg/releases/download/v0.2.4/fg_0.2.4_linux_amd64.tar.gz"
-    sha256 "9dd4ee1049da6faf152e8f540cf11d56c024c8ffb08ebcac597fe0de3972fecf"
-  end
-  if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/aegoroff/fg/releases/download/v0.2.4/fg_0.2.4_linux_armv7.tar.gz"
-    sha256 "f088f93ea79d96c90e4d73b4a8c067d8f41ff35952e730af681a29dda9e982ca"
-  end
-  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/aegoroff/fg/releases/download/v0.2.4/fg_0.2.4_linux_arm64.tar.gz"
-    sha256 "e6b3f5fee3628e5e9c5bea1cef978a440c8241d2d3b527d0b0ee8fe997ba2239"
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/aegoroff/fg/releases/download/v0.2.5/fg_0.2.5_linux_amd64.tar.gz"
+      sha256 "9b00e14735f91769edbdb136f8274bae962c5d5ab41db0af2f9fd089baa50cd2"
+    end
+    if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+      url "https://github.com/aegoroff/fg/releases/download/v0.2.5/fg_0.2.5_linux_armv7.tar.gz"
+      sha256 "9bf20ada5abe00e6993e3d273c05829504cbfaa14761a2361aa243556f15ae09"
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/aegoroff/fg/releases/download/v0.2.5/fg_0.2.5_linux_arm64.tar.gz"
+      sha256 "67420a30240688b9bc174a79eb1a0c9c1ab7174c120cb1cfe60701c2e1bcc1b9"
+    end
   end
 
   def install
-    bin.install "fg"
+    bin.install "fgr"
   end
 
   test do
