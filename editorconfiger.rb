1bf6f4b61a69541ae214e2fd12a6c9ba5dfa9a26
aegoroff <egoroff@email.com>
2022-03-15 06:31:22 +0000

    release: v0.2.5

--- editorconfiger.rb
+++ editorconfiger.rb
@@ -4,20 +4,20 @@
 class Editorconfiger < Formula
   desc "Plain tool to validate and compare .editorconfig files"
   homepage "https://github.com/aegoroff/editorconfiger"
-  version "0.2.4"
+  version "0.2.5"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/aegoroff/editorconfiger/releases/download/v0.2.4/editorconfiger-v0.2.4-x86_64-apple-darwin.tar.gz"
-      sha256 "71fda7a3f391f1223b64fbed16e9d8e9feff4e28f99769fb37dad03590dc05ea"
+      url "https://github.com/aegoroff/editorconfiger/releases/download/v0.2.5/editorconfiger-v0.2.5-x86_64-apple-darwin.tar.gz"
+      sha256 "66fe37d2a208cadd0c607dd3dc8e2d8f1810a329c245fe55e30450af5589016c"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/aegoroff/editorconfiger/releases/download/v0.2.4/editorconfiger-v0.2.4-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "38cde60bc44a87dd0e793c5bd188abe159c6de65834ecc677e3c8109a11027fb"
+      url "https://github.com/aegoroff/editorconfiger/releases/download/v0.2.5/editorconfiger-v0.2.5-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "822f566c66d9f1121fd724aeb725af9bfbaa82bbb5d0141084f6ee73b281f694"
     end
   end
 
