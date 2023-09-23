24a33889463bf017871f6fc4514bdc3070253b41
Alexander Egorov <egoroff@gmail.com>
2023-09-23 19:55:11 +0300

    Brew formula update for copyto version v1.2.10

--- copyto.rb
+++ copyto.rb
@@ -9,17 +9,17 @@ class Copyto < Formula
   license "MIT"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/aegoroff/copyto/releases/download/v1.2.10/copyto_1.2.10_darwin_arm64.tar.gz"
-      sha256 "4edc2fb8e2abd2a948d9dcf2c3822d9defb67ec50ed03d23b7df9db88727f03c"
+    if Hardware::CPU.intel?
+      url "https://github.com/aegoroff/copyto/releases/download/v1.2.10/copyto_1.2.10_darwin_amd64.tar.gz"
+      sha256 "031f1c254b3046906e2ea7f5fed3773c18a060baca65070a0a5ce98babbd88ad"
 
       def install
         bin.install "copyto"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/aegoroff/copyto/releases/download/v1.2.10/copyto_1.2.10_darwin_amd64.tar.gz"
-      sha256 "25c7b37fb0b35ba681dbde2dd3655e0a7cd8248d837c943f47fcad52e0bcd9ae"
+    if Hardware::CPU.arm?
+      url "https://github.com/aegoroff/copyto/releases/download/v1.2.10/copyto_1.2.10_darwin_arm64.tar.gz"
+      sha256 "bb3e22b8ccb9ded29f26a14bcead0ce2f1da4b97a4775c9d99ac26ec7000e0e2"
 
       def install
         bin.install "copyto"
@@ -28,17 +28,25 @@ class Copyto < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/aegoroff/copyto/releases/download/v1.2.10/copyto_1.2.10_linux_amd64.tar.gz"
-      sha256 "73a6392765036e707bad453b98c32bcba4797495cbcb69717d75c793201ddf4c"
+    if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+      url "https://github.com/aegoroff/copyto/releases/download/v1.2.10/copyto_1.2.10_linux_armv7.tar.gz"
+      sha256 "cb2271b040bb4f3c0fcde48d92167eaa129154700e35210c49f6f506e1dd0043"
 
       def install
         bin.install "copyto"
       end
     end
-    if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/aegoroff/copyto/releases/download/v1.2.10/copyto_1.2.10_linux_armv7.tar.gz"
-      sha256 "89000d8406b56cb752693aedd90689273c794d13a1e6aeccfd5cc63b0aa674a2"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/aegoroff/copyto/releases/download/v1.2.10/copyto_1.2.10_linux_arm64.tar.gz"
+      sha256 "5f2540981de0d490bd898f558a4f79ba908acff1209f5a8f5152b6bbfc501439"
+
+      def install
+        bin.install "copyto"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/aegoroff/copyto/releases/download/v1.2.10/copyto_1.2.10_linux_amd64.tar.gz"
+      sha256 "3ba48b213134a2cc06920fb4f272bab3f97f6a8377584d8af521344c1c19e7ac"
 
       def install
         bin.install "copyto"
