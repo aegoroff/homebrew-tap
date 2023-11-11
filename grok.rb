7484cf2d9547f363fcb6c323506cf7126531971a
aegoroff <egoroff@email.com>
2023-11-11 07:46:28 +0000

    release: 0.2.14

--- grok.rb
+++ grok.rb
@@ -4,20 +4,20 @@
 class Grok < Formula
   desc "Regular expressions macro engine. GROK is a tool like UNIX grep on steroids"
   homepage "https://github.com/aegoroff/grok"
-  version "0.2.13"
+  version "0.2.14"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/aegoroff/grok/releases/download/0.2.13/grok-0.2.13-x86_64-apple-darwin.tar.gz"
-      sha256 "fd680f206a1541dcf29537980769659b2be73c26fecd23866df523bd535276ed"
+      url "https://github.com/aegoroff/grok/releases/download/0.2.14/grok-0.2.14-x86_64-apple-darwin.tar.gz"
+      sha256 "f20cb9fb0e5fb9beee296250423955aa4f79a5844bbc5899e4e1c0ca9bec3ea6"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/aegoroff/grok/releases/download/0.2.13/grok-0.2.13-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "c8d00a6f3c84482feebfcf7dcec1abb6a9d47dc91f62c56c5e8befc78631e2f8"
+      url "https://github.com/aegoroff/grok/releases/download/0.2.14/grok-0.2.14-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "642450a4d56e4befdc1099deabb596cf0a5be0f1cdfd8452ecd9b64bf8faeac0"
     end
   end
 
