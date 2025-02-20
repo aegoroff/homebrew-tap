a221d012603662e5a062b9051f51cb689c1f1b98
aegoroff <egoroff@email.com>
2025-02-20 15:34:06 +0000

    release: 0.4.13

--- editorconfiger.rb
+++ editorconfiger.rb
@@ -4,24 +4,24 @@
 class Editorconfiger < Formula
   desc "Plain tool to validate and compare .editorconfig files"
   homepage ""
-  version "0.4.12"
+  version "0.4.13"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/aegoroff/editorconfiger/releases/download/0.4.12/editorconfiger-0.4.12-x86_64-apple-darwin.tar.gz"
-      sha256 "d59e4dc0808662092046a3322bc1837c03b191725665efe8fd751448326efd2e"
+      url "https://github.com/aegoroff/editorconfiger/releases/download/0.4.13/editorconfiger-0.4.13-x86_64-apple-darwin.tar.gz"
+      sha256 "110b892b8e2e270a880961421723b699b8d8d62cb996a240cce21e59c4fea48c"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/aegoroff/editorconfiger/releases/download/0.4.12/editorconfiger-0.4.12-aarch64-apple-darwin.tar.gz"
-      sha256 "4890c8140cf8de32c3e7aea4750b225af6dc9bfe8b9cc77d469471cc147e9110"
+      url "https://github.com/aegoroff/editorconfiger/releases/download/0.4.13/editorconfiger-0.4.13-aarch64-apple-darwin.tar.gz"
+      sha256 "9d9e71a50e1e25f0320140c902e84309ed794fa77ca9f2a86dc20d6d54dfe629"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/aegoroff/editorconfiger/releases/download/0.4.12/editorconfiger-0.4.12-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "4924bd00739d2bacc086c230472cb39587ae90893190a47edaa4374d79be800e"
+      url "https://github.com/aegoroff/editorconfiger/releases/download/0.4.13/editorconfiger-0.4.13-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "f23c804a43c91fd4c1b050c37dc44647145c97f187841d7342ce3050e9d2af62"
     end
   end
 
